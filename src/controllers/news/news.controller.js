59d6f4aaac09c851abeb476099a8bbd2bbf0dd43
Sevinch <erkinovasevinch38@gmail.com>
2025-07-11 12:41:57 +0500

    news controller add funksiya qo'shildi

--- src/controllers/news/news.controller.js
+++ src/controllers/news/news.controller.js
@@ -1,7 +1,14 @@
 class NewsController {
+
   static getAll = (req , res) => {
 		res.status(201).json({success:true,msg:"Hello Word !"})
 	}
+
+	static add = (req, res)=> {
+		const {title , desc} = req.body
+		res.status(201).json({success:true,data:{title,desc}})
+	}
+
 }
 
 module.exports = {NewsController}
